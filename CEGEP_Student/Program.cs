59591219b769f99087e4c9f12d9af538669e7dfd
y3lousso <y3lousso@gmail.com>
2018-02-26 22:37:54 -0500

    Update student side - Ready for Release V1

--- CEGEP_Student/Program.cs
+++ CEGEP_Student/Program.cs
@@ -32,12 +32,14 @@ namespace CEGEP_Student
 
             InterfaceOrienteObjet i = new InterfaceOrienteObjet();
 
-            for (int x = 0; x < 20; x++)
+            for (int x = 0; x < 5; x++)
             {
                 int posX = 20 * x;
                 int posY = (20 * x) + x;               
                 i.DessinerRectangle(new Vector2(posX,posY), 50, 70); 
-                i.DessinerCercle(new Vector2(posX,posY+50), 20);
+                i.DessinerCercle(new Vector2(100+posX,posY), 20);
+                i.DessinerCarre(new Vector2(posX, posY + 150), 30);
+                i.DessinerEllipse(new Vector2(posX, posY + 250), 20, 50);
                 Thread.Sleep(500);
                 Canvas.instance.Show();
             }
