1e5b604fdf78d546fc0f5a2435724ff8c0dcc967
Folgar <loick1994@hotmail.fr>
2018-03-01 23:50:48 +0100

    Revert "[AQ-variables]"
    This reverts commit df47dc50c44552d074e0e75cb48c3fe9d48c9164.

--- AMCP/Ellipse.cs
+++ AMCP/Ellipse.cs
@@ -9,8 +9,8 @@ namespace AMCP
     public class Ellipse : Forme
     {
         public Vector2 position { get; set; }
-        float PetitRayon { get; set; } //TODO pas de majuscule cf 6.6
-        float GrandRayon { get; set; } //TODO pas de majuscule cf 6.6
+        float PetitRayon { get; set; }
+        float GrandRayon { get; set; }
             
         internal Ellipse(Vector2 position, int rayon1, int rayon2)
         {
