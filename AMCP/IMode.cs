b26a5e43d9267ba15515ddfcf4c4d72321144f48
Battiston <aurelie.battiston@efrei.net>
2018-03-02 00:35:23 -0500

    Revert [AQ]Gestion d'erreur

--- AMCP/IMode.cs
+++ AMCP/IMode.cs
@@ -23,7 +23,6 @@ namespace AMCP
             }
             else
             {
-                //TODO chaine de description en Anglais
                 throw new Exception("Can't create multiple instance of IMode");
             }
         }
