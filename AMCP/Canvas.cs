1e1fa5d22b52887c45ff1bece2e8333a370fd582
Battiston <aurelie.battiston@efrei.net>
2018-03-02 00:34:38 -0500

    Revert [AQ]Gestion d'erreur

--- AMCP/Canvas.cs
+++ AMCP/Canvas.cs
@@ -29,7 +29,6 @@ namespace AMCP
             }
             else
             {
-            //TODO chaine de description en Anglais
                 throw new Exception("Can't create multiple instance of Canvas");
             }
         }
