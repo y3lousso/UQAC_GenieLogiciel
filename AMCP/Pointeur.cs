784035c0abdee9b982ad56b85c125390d4b6ec38
Battiston <aurelie.battiston@efrei.net>
2018-03-02 16:04:12 -0500

    [AQ]Respect CON

--- AMCP/Pointeur.cs
+++ AMCP/Pointeur.cs
@@ -12,6 +12,7 @@ namespace AMCP
         int Id { get; set; } 
         //TODO deux int dans le CON
         Vector2 Position { get; set; } 
+         //TODO n'est pas le même nom que dans le CON, manque le paramètre
         int TaillePointeur { get; set; } 
         string Couleur { get; set; } 
 
