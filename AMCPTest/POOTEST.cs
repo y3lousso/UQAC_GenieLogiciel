9b170834d32788f6a9673fac91ff3631f0862374
AlexSinmy <alexandre.mesny@gmail.com>
2018-03-19 12:16:53 -0500

    add forme test

--- AMCPTest/POOTEST.cs
+++ AMCPTest/POOTEST.cs
@@ -27,16 +27,50 @@ namespace AMCPTest
             Ellipse testEllipse = null;
             testEllipse = i.DessinerCercle(0 + posX, posY, -1);
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(null, testEllipse);
-            
         }
 
         [TestMethod]
-        public void POODessinerRectangle50x70()
+        public void POODessinerRectangle()
         {
             i = new InterfaceOrienteeObjet();
             int posX = 20;
             int posY = 20;
-            i.DessinerRectangle(posX, posY, 50, 70);
+            Polygone testPolygone = null;
+            testPolygone = i.DessinerRectangle(posX, posY, 50, 70);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(null, testPolygone);
+        }
+
+        [TestMethod]
+        public void POODessinerEtoile()
+        {
+            i = new InterfaceOrienteeObjet();
+            int posX = 20;
+            int posY = 20;
+            Polygone testPolygone = null;
+            testPolygone = i.DessinerEtoile(posX, posY + 100, 40, 80, 5);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(null, testPolygone);
+        }
+
+        [TestMethod]
+        public void POODessinerEllipse()
+        {
+            i = new InterfaceOrienteeObjet();
+            int posX = 20;
+            int posY = 20;
+            Ellipse testEllipse = null;
+            testEllipse = i.DessinerEllipse(posX, posY + 250, 20, 50);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(null, testEllipse);
+        }
+
+        [TestMethod]
+        public void POODessinerTriangle()
+        {
+            i = new InterfaceOrienteeObjet();
+            int posX = 20;
+            int posY = 20;
+            Polygone testPolygone = null;
+            testPolygone = i.DessinerTriangle(posX, posY, 20);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(null, testPolygone);
         }
 
         /*[TestMethod]
--- AMCPTest/SEQTEST.cs
+++ AMCPTest/SEQTEST.cs
@@ -7,13 +7,70 @@ namespace AMCPTest
     [TestClass]
     public class SEQTEST
     {
-        /*[TestMethod]
-        public void DessinerRectangle50x70()
+        InterfaceSequencielle i;
+
+        [TestInitialize()]
+        public void Initialize()
         {
-            InterfaceSequencielle i = new InterfaceSequencielle();
-            i.DessinerRectangle(20, 20, 50, 70);
-            i.Afficher();
-        }*/
-        
+            new PrivateType(typeof(IMode)).SetStaticField("instance", null);
+            new PrivateType(typeof(Canvas)).SetStaticField("instance", null);
+        }
+
+
+        [TestMethod]
+        public void SEQDessinerCercle()
+        {
+            i = new InterfaceSequencielle();
+            int testEllipse = -1;
+            testEllipse = i.DessinerCercle(20, 20, -1);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(-1, testEllipse);
+        }
+
+        [TestMethod]
+        public void SEQDessinerRectangle()
+        {
+            i = new InterfaceSequencielle();
+            int testPolygone = -1;
+            testPolygone = i.DessinerRectangle(20, 20, 50, 70);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(-1, testPolygone);
+        }
+
+        [TestMethod]
+        public void SEQDessinerEtoile()
+        {
+            i = new InterfaceSequencielle();
+            int testPolygone = -1;
+            testPolygone = i.DessinerEtoile(20, 120, 40, 80, 5);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(-1, testPolygone);
+        }
+
+        [TestMethod]
+        public void SEQDessinerEllipse()
+        {
+            i = new InterfaceSequencielle();
+            int testEllipse = -1;
+            testEllipse = i.DessinerEllipse(20, 270, 20, 50);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(-1, testEllipse);
+        }
+
+        [TestMethod]
+        public void SEQDessinerTriangle()
+        {
+            i = new InterfaceSequencielle();
+            int testPolygone = -1;
+            testPolygone = i.DessinerTriangle(100, 100, 200);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(-1, testPolygone);
+            
+        }
+
+        [TestMethod]
+        public void SEQTournerPolygone()
+        {
+            i = new InterfaceSequencielle();
+            int testPolygone = -1;
+            testPolygone = i.DessinerTriangle(20, 20, 20);
+            i.Tourner(testPolygone, -300000);
+        }
+
     }
 }
